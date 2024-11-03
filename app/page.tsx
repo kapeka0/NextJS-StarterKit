4e677f503edc6a6fb64dd181bd39645f3380664c
KariX Mamon <karimcarreno5@gmail.com>
2024-11-03 23:28:44 +0100

    added btn for landing primary preview

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+import { ThemeToggle } from "@/components/global/ThemeToggle";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>Start building !!!</h1>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-3">
+      <h1 className="text-4xl font-semibold">Start building !!!</h1>
+      <div className="flex gap-2">
+        <Button>Click Me</Button>
+        <ThemeToggle />
+      </div>
+    </div>
+  );
 }
