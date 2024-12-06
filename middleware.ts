6a54915877460c19377288839f2fe400d16ab3cc
KariX Mamon <karimcarreno5@gmail.com>
2024-12-06 14:23:56 +0100

    Changed middleware

--- middleware.ts
+++ middleware.ts
@@ -7,17 +7,23 @@ import { i18nConfig } from "./i18n/i18nConfig";
 const handleI18nRouting = createMiddleware(routing);
 
 export async function middleware(request: NextRequest) {
+  // Redirect all routes to the setted locale or the default one
   if (
     !routing.locales.includes(request.nextUrl.pathname.split("/")[1] as any)
   ) {
+    console.log(
+      `${request.nextUrl.origin}/${
+        request.cookies.get("NEXT_LOCALE")?.value || i18nConfig.defaultLocale
+      }${request.nextUrl.pathname}`
+    );
     return NextResponse.redirect(
-      new URL(
-        request.cookies.get("NEXT_LOCALE")?.value || i18nConfig.defaultLocale,
-        request.nextUrl
-      )
+      `${request.nextUrl.origin}/${
+        request.cookies.get("NEXT_LOCALE")?.value || i18nConfig.defaultLocale
+      }${request.nextUrl.pathname}`
     );
   }
   const response = handleI18nRouting(request);
+
   return response;
 }
 
