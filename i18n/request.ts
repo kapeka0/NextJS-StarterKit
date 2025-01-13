2fc428e6b55834e50545ff4b6188ae6127b6d7ff
KariX Mamon <karimcarreno5@gmail.com>
2025-01-13 13:13:28 +0100

    updated i18n request locale

--- i18n/request.ts
+++ i18n/request.ts
@@ -9,6 +9,7 @@ export default getRequestConfig(async ({ requestLocale }) => {
     notFound();
   }
   return {
+    locale,
     messages: (await import(`../messages/${locale}.json`)).default,
   };
 });
