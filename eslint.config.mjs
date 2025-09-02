d579cc6d2455f888265a24d6f828497211e327de
kapeka0 <karimcarreno5@gmail.com>
2025-09-02 19:30:57 +0200

    updated eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,11 @@
 /* eslint-disable import/no-anonymous-default-export */
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
 import typescriptEslintEslintPlugin from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
 import prettier from "eslint-plugin-prettier";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -37,7 +37,11 @@ export default [
             },
             {
               group: ["next/link"],
-              message: "Import from 'next/navigation' instead.",
+              message: "Import from '@/i18n/routing' instead.",
+            },
+            {
+              group: ["next/navigation"],
+              message: "Import from '@/i18n/routing' instead.",
             },
           ],
         },
